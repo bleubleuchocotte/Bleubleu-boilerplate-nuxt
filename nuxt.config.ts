bccc9c3538e01098b02ba9f7fbe95b584f0f2e76
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-01-24 10:29:27 +0100

    feat(nuxt): introduce device module to identify the requesting device

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@vueuse/nuxt", "@pinia/nuxt", "nuxt-simple-sitemap", "nuxt-simple-robots"],
+	modules: ["@vueuse/nuxt", "@pinia/nuxt", "nuxt-simple-sitemap", "nuxt-simple-robots", "@nuxtjs/device"],
 	css: ["@/assets/styles/reset.css", "@/assets/styles/1-base/_base-default.scss"],
 	vite: {
 		css: {
