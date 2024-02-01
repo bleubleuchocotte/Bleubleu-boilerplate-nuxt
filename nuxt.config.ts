58776dd9445941bbe90a2adc8343c99786da7b8a
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-02-01 15:58:30 +0100

    chore(ci): investigate typecheck discrepancy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@vueuse/nuxt", "@pinia/nuxt", "nuxt-simple-sitemap", "nuxt-simple-robots", "@nuxtjs/device"],
+	modules: [
+		"@vueuse/nuxt",
+		"@pinia/nuxt",
+		"nuxt-simple-sitemap",
+		"nuxt-simple-robots",
+		"@nuxtjs/device",
+	],
 	css: ["@/assets/styles/reset.css", "@/assets/styles/1-base/_base-default.scss"],
 	vite: {
 		css: {
