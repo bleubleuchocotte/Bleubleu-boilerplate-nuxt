8a5fbf82e4d9e6b461fa6d51e6900e4896a67e60
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-08-29 10:45:59 +0200

    chore: add a compatibility date in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+
 	modules: [
 		"@vueuse/nuxt",
 		"@nuxtjs/sitemap",
 		"@nuxtjs/robots",
 	],
+
 	css: ["@/assets/styles/reset.css", "@/assets/styles/1-base/_base-default.scss"],
+
 	vite: {
 		css: {
 			preprocessorOptions: {
@@ -16,7 +19,10 @@ export default defineNuxtConfig({
 			},
 		},
 	},
+
 	robots: {
 		disallow: ["/legal-notice"],
 	},
+
+	compatibilityDate: "2024-08-29",
 });
