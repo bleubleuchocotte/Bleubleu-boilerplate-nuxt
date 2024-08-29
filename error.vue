37d33aaad457263bf35b58751243e38c7ca0bb06
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-08-29 11:33:20 +0200

    feat: move seo and head to nuxt-config

--- error.vue
+++ error.vue
@@ -7,40 +7,6 @@ type ComponentProps = {
 
 const props = defineProps<ComponentProps>();
 
-const request = useRequestURL();
-
-// On place ici les propriétés non-réactives
-useServerSeoMeta({
-	ogImage: `${request.origin}/images/og-image.png`,
-	ogUrl: request.origin + request.pathname,
-
-	titleTemplate: "%s | Le titre du site",
-	ogTitle: "%s | Le titre du site",
-
-	description: "La description du site.",
-	ogDescription: "La description du site.",
-
-	twitterCard: "summary_large_image",
-
-	themeColor: "#FFFFFF",
-	colorScheme: "light dark",
-
-	creator: "BleuBleu Chocotte",
-});
-
-useHead({
-	htmlAttrs: {
-		lang: "en",
-	},
-	link: [
-		{
-			rel: "icon",
-			type: "image/png",
-			href: `${request.origin}/favicon-32x32.ico`,
-		},
-	],
-});
-
 useSeoMeta({
 	title: `${props.error.statusCode} Error`,
 });
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,46 @@ export default defineNuxtConfig({
 
 	css: ["@/assets/styles/reset.css", "@/assets/styles/1-base/_base-default.scss"],
 
+	app: {
+		head: {
+			title: "Le titre du site",
+			htmlAttrs: {
+				lang: "fr",
+			},
+			link: [{
+				rel: "icon",
+				type: "image/png",
+				href: `/favicon-32x32.ico`,
+			}],
+			meta: [
+				{
+					name: "description",
+					content: "La description du site",
+				},
+				{
+					name: "creator",
+					content: "Bleubleu Chocotte",
+				},
+				{
+					name: "twitter:card",
+					content: "summary_large_image",
+				},
+				{
+					property: "og:title",
+					content: "Le titre du site",
+				},
+				{
+					property: "og:description",
+					content: "La description du site",
+				},
+				{
+					property: "og:image",
+					content: "/images/og-image.png",
+				},
+			],
+		},
+	},
+
 	vite: {
 		css: {
 			preprocessorOptions: {
