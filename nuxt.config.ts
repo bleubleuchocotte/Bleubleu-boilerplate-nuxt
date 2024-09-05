09b5c2f71e46f9092c3fe1951ef1cbeafef07b08
Thomas <thomas.auffroy@gmail.com>
2024-09-05 16:53:52 +0200

    chore: disable devtools by default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
+	devtools: { enabled: false },
 
 	modules: ["@vueuse/nuxt", "@nuxtjs/sitemap", "@nuxtjs/robots", "@nuxtjs/device"],
 
